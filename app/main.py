960eaa411136baa892eddbcea909da27833d3469
Ikotun Collins <danlogan2003@gmail.com>
2023-07-08 21:04:29 +0100

    protected routes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
-from fastapi import FastAPI, HTTPException, status, Response, Depends, APIRouter
-from pydantic import BaseModel
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from fastapi import FastAPI
 from . import models
-from .database import SessionLocal, engine
-from sqlalchemy.orm import Session
-from . import schemas
-from . import utils
+from .database import engine
 from .routers import post, user, auth
 
 
@@ -14,13 +8,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#dependency
-def get_db():
-    db = SessionLocal()
-    try :
-        yield db
-    finally : 
-        db.close()
 
 app.include_router(post.router)
 app.include_router(user.router)
