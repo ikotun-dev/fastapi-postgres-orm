0a4b375b803f20b708a0021c3eff58e59bb88ebc
Ikotun Collins <danlogan2003@gmail.com>
2023-07-10 17:15:38 +0100

    alembic

---
+++ alembic/versions/d9cd3a11a43d_post_table.py
@@ -0,0 +1,24 @@
+"""post table
+
+Revision ID: d9cd3a11a43d
+Revises: 
+Create Date: 2023-07-10 15:38:31.137900
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd9cd3a11a43d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
